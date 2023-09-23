445e349b97c72bf6e23f8980215e586118c9e37d
Jonathan Araujo <jonathan@wicadu.com>
2023-09-23 10:09:00 -0300

    Integrate database to running process app [index.ts]

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import 'dotenv/config'
-import './database'
-
 import express from 'express'
+
+import Database from './database'
 
 const app = express()
 
-app.listen(process.env.PORT, () => console.log(`Server running on PORT: ${process.env.PORT}`))
+const db = new Database(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`)
+
+app.use(express.json())
+
+db.connect().then((a) => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT: ${process.env.PORT}`)
+  })
+})
+

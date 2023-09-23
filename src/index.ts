2bfdc622bd23b2ab7715df9f69ab4a05695a0cae
Jonathan Araujo <jonathan@wicadu.com>
2023-09-23 10:11:50 -0300

    Include usersRouter into index and install HttpStatusCodes dependency

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import 'dotenv/config'
 import express from 'express'
 
 import Database from './database'
+import usersRouter from './users/users.routers'
 
 const app = express()
 
 const db = new Database(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`)
 
 app.use(express.json())
+app.use(usersRouter)
 
 db.connect().then((a) => {
   app.listen(process.env.PORT, () => {
